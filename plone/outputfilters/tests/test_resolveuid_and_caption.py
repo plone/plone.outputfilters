d06376bd22c69fc7de3c42daba66b3b9fb51fab0
Timo Stollenwerk <contact@timostollenwerk.net>
2013-03-19 09:13:43 +0100

    Fix test failure.

--- plone/outputfilters/tests/test_resolveuid_and_caption.py
+++ plone/outputfilters/tests/test_resolveuid_and_caption.py
@@ -254,7 +254,7 @@ alert(1);
 
     def test_image_captioning_absolutizes_uncaptioned_image(self):
         text_in = """<img src="/image.jpg" />"""
-        text_out = """<img src="http://nohost/plone/image.jpg" alt="Image" title="Image" />"""
+        text_out = """<img src="http://nohost/plone/image.jpg" alt="My caption" title="Image" />"""
         self._assertTransformsTo(text_in, text_out)
 
     def test_image_captioning_absolute_path(self):
