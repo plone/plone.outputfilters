305dd1b081e3a8c034719cd9b2bfd7b9c7adc83c
Tom Gross <itconsense@gmail.com>
2015-01-21 14:19:40 +0000

    fix image URLs to make tests pass for p.a.contenttypes

--- plone/outputfilters/tests/test_resolveuid_and_caption.py
+++ plone/outputfilters/tests/test_resolveuid_and_caption.py
@@ -311,15 +311,15 @@ alert(1);
 
         text_in = """<img class="captioned" src="private/image.jpg"/>"""
         text_out = """<dl style="width:500px;" class="captioned">
-<dt><img src="http://nohost/plone/private/image.jpg/image" alt="Image" title="Image" height="331" width="500" /></dt>
+<dt><img src="http://nohost/plone/private/image.jpg/@@images/....jpeg" alt="Image" title="Image" height="331" width="500" /></dt>
  <dd class="image-caption" style="width:500px;">My private image caption</dd>
 </dl>"""
         self._assertTransformsTo(text_in, text_out)
 
     def test_image_captioning_relative_path_scale(self):
-        text_in = """<img class="captioned" src="image.jpg/image_thumb"/>"""
+        text_in = """<img class="captioned" src="image.jpg/@@images/image/thumb"/>"""
         text_out = """<dl style="width:128px;" class="captioned">
-<dt><a rel="lightbox" href="/plone/image.jpg"><img src="http://nohost/plone/image.jpg/image_thumb" alt="Image" title="Image" height="84" width="128" /></a></dt>
+<dt><a rel="lightbox" href="/plone/image.jpg"><img src="http://nohost/plone/image.jpg/@@images/....jpeg" alt="Image" title="Image" height="84" width="128" /></a></dt>
  <dd class="image-caption" style="width:128px;">My caption</dd>
 </dl>"""
         self._assertTransformsTo(text_in, text_out)
@@ -333,9 +333,9 @@ alert(1);
         self._assertTransformsTo(text_in, text_out)
 
     def test_image_captioning_resolveuid_scale(self):
-        text_in = """<img class="captioned" src="resolveuid/%s/image_thumb"/>""" % self.UID
+        text_in = """<img class="captioned" src="resolveuid/%s/@@images/image/thumb"/>""" % self.UID
         text_out = """<dl style="width:128px;" class="captioned">
-<dt><a rel="lightbox" href="/plone/image.jpg"><img src="http://nohost/plone/image.jpg/image_thumb" alt="Image" title="Image" height="84" width="128" /></a></dt>
+<dt><a rel="lightbox" href="/plone/image.jpg"><img src="http://nohost/plone/image.jpg/@@images/....jpeg" alt="Image" title="Image" height="84" width="128" /></a></dt>
  <dd class="image-caption" style="width:128px;">My caption</dd>
 </dl>"""
         self._assertTransformsTo(text_in, text_out)
@@ -405,9 +405,9 @@ alert(1);
         self._assertTransformsTo(text_in, text_out)
 
     def test_image_captioning_preserves_existing_links(self):
-        text_in = """<a href="/xyzzy" class="link"><img class="image-left captioned" src="image.jpg/image_thumb"/></a>"""
+        text_in = """<a href="/xyzzy" class="link"><img class="image-left captioned" src="image.jpg/@@images/image/thumb"/></a>"""
         text_out = """<a href="/xyzzy" class="link"><dl style="width:128px;" class="image-left captioned">
-<dt><img src="http://nohost/plone/image.jpg/image_thumb" alt="Image" title="Image" height="84" width="128" /></dt>
+<dt><img src="http://nohost/plone/image.jpg/@@images/....jpeg" alt="Image" title="Image" height="84" width="128" /></dt>
  <dd class="image-caption" style="width:128px;">My caption</dd>
 </dl>
 </a>"""
