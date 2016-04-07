a183f097a1cfdade7564b28973d1c189b1509b1d
Johannes Raggam <thetetet@gmail.com>
2016-04-07 02:04:53 +0200

    Explicitly exclude ``mailto:`` links from being UID-resolved, Fix test isolation problem.

--- plone/outputfilters/filters/resolveuid_and_caption.py
+++ plone/outputfilters/filters/resolveuid_and_caption.py
@@ -324,6 +324,7 @@ class ResolveUIDAndCaptionFilter(SGMLParser):
                 scheme = urlsplit(href)[0]
                 if not scheme and not href.startswith('/') \
                         and not href.startswith('mailto<') \
+                        and not href.startswith('mailto:') \
                         and not href.startswith('tel:') \
                         and not href.startswith('#'):
                     obj, subpath, appendix = self.resolve_link(href)
