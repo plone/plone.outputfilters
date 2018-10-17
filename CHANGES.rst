3a329d0cb58aba641061df1ab306e0d1d30ea1e3
Jens W. Klein <jk@kleinundpartner.at>
2018-10-17 11:35:38 +0200

    mark version as feature change

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-3.0.6 (unreleased)
+3.1.x (unreleased)
 ------------------
 
 Breaking changes:
--- setup.py
+++ setup.py
@@ -5,60 +5,64 @@ from setuptools import setup
 import os
 
 
-version = '3.0.6.dev0'
+version = '3.1.0.dev0'
 
-setup(name='plone.outputfilters',
-      version=version,
-      description=("Transformations applied to HTML in "
-                   "Plone text fields as they are rendered"),
-      long_description=(
-          open("README.rst").read() + "\n" +
-          open(os.path.join("plone", "outputfilters",
-                            "README.rst")).read() + "\n" +
-          open("CHANGES.rst").read()),
-      # Get more strings from
-      # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          "Framework :: Plone",
-          "Framework :: Plone :: 5.0",
-          "Framework :: Plone :: 5.1",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.6",
-      ],
-      keywords='plone transform filter uid caption',
-      author='David Glick, Plone Foundation',
-      author_email='davidglick@groundwire.org',
-      url='http://github.com/plone/plone.outputfilters',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'Products.CMFCore',
-          'Products.GenericSetup',
-          'Products.MimetypesRegistry',
-          'Products.PortalTransforms>=2.0a1',
-          'setuptools',
-          'six',
-          'unidecode',
-          'beautifulsoup4',
-          'lxml',
-      ],
-      extras_require={
-          'test': [
-              'plone.app.contenttypes',
-              'plone.app.testing',
-              'plone.app.robotframework',
-              'plone.namedfile',
-          ],
-      },
-      entry_points="""
+setup(
+    name='plone.outputfilters',
+    version=version,
+    description=(
+        "Transformations applied to HTML in "
+        "Plone text fields as they are rendered"
+    ),
+    long_description=(
+        open("README.rst").read()
+        + "\n"
+        + open(os.path.join("plone", "outputfilters", "README.rst")).read()
+        + "\n"
+        + open("CHANGES.rst").read()
+    ),
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords='plone transform filter uid caption',
+    author='David Glick, Plone Foundation',
+    author_email='davidglick@groundwire.org',
+    url='http://github.com/plone/plone.outputfilters',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Products.CMFCore',
+        'Products.GenericSetup',
+        'Products.MimetypesRegistry',
+        'Products.PortalTransforms>=2.0a1',
+        'setuptools',
+        'six',
+        'unidecode',
+        'beautifulsoup4',
+        'lxml',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.contenttypes',
+            'plone.app.testing',
+            'plone.app.robotframework',
+            'plone.namedfile',
+        ]
+    },
+    entry_points="""
       # -*- Entry points: -*-
 
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      )
+)
