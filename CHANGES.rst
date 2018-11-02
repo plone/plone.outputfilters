5f7b3b552a096489de24c39b69e1f1327ebf7a64
esteele <eric@esteele.net>
2018-11-02 02:23:48 -0400

    Preparing release 3.1.0
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-3.1.x (unreleased)
+3.1.0 (2018-11-02)
 ------------------
 
 New features:
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 import os
 
 
-version = '3.1.0.dev0'
+version = '3.1.0'
 
 setup(
     name='plone.outputfilters',
