11269fd3f9b965205f59c781b8961d53829a6b74
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-01-04 03:58:40 +0000

    Fix tests.

--- plone/outputfilters/tests/test_resolveuid_and_caption.py
+++ plone/outputfilters/tests/test_resolveuid_and_caption.py
@@ -371,27 +371,27 @@ alert(1);
 
     def test_iframe_resolveuid(self):
         text_in = """<iframe src="resolveuid/%s"/>""" % self.UID
-        text_out = """<iframe src="http://nohost/plone/image.jpg/@@images/...jpeg"/>"""
+        text_out = """<iframe src="http://nohost/plone/image.jpg"/>"""
         self._assertTransformsTo(text_in, text_out)
 
     def test_video_resolveuid(self):
         text_in = """<video src="resolveuid/%s"/>""" % self.UID
-        text_out = """<video src="http://nohost/plone/image.jpg/@@images/...jpeg"/>"""
+        text_out = """<video src="http://nohost/plone/image.jpg"/>"""
         self._assertTransformsTo(text_in, text_out)
 
     def test_audio_resolveuid(self):
         text_in = """<audio src="resolveuid/%s"/>""" % self.UID
-        text_out = """<audio src="http://nohost/plone/image.jpg/@@images/...jpeg"/>"""
+        text_out = """<audio src="http://nohost/plone/image.jpg"/>"""
         self._assertTransformsTo(text_in, text_out)
 
     def test_source_resolveuid(self):
         text_in = """<video><source src="resolveuid/%s"/></video>""" % self.UID
-        text_out = """<video><source src="http://nohost/plone/image.jpg/@@images/...jpeg"/></video>"""
+        text_out = """<video><source src="http://nohost/plone/image.jpg"/></video>"""
         self._assertTransformsTo(text_in, text_out)
 
     def test_source_resolveuid_srcset(self):
         text_in = """<video><source srcset="resolveuid/%s" mimetype="video/mp4"/></video>""" % self.UID
-        text_out = """<video><source srcset="http://nohost/plone/image.jpg/@@images/...jpeg" mimetype="video/mp4"/></video>"""
+        text_out = """<video><source srcset="http://nohost/plone/image.jpg" mimetype="video/mp4"/></video>"""
         self._assertTransformsTo(text_in, text_out)
 
     def test_image_captioning_resolveuid_no_scale_plone_namedfile(self):
