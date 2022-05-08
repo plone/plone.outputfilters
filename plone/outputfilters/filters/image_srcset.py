785b0fe4db7bbfc413f7375a2e347a461eda3440
MrTango <md@derico.de>
2022-05-08 21:00:42 +0300

    set w parameter in srcset definitions

--- plone/outputfilters/filters/image_srcset.py
+++ plone/outputfilters/filters/image_srcset.py
@@ -51,7 +51,7 @@ class ImageSrcsetFilter(object):
         self.request = request
 
     @property
-    def image_scales(self):
+    def allowed_scales(self):
         registry = getUtility(IRegistry)
         settings = registry.forInterface(IImagingSchema, prefix="plone", check=False)
         return settings.allowed_sizes
@@ -61,6 +61,24 @@ class ImageSrcsetFilter(object):
         registry = getUtility(IRegistry)
         settings = registry.forInterface(IImagingSchema, prefix="plone", check=False)
         return settings.image_srcsets
+
+    def get_scale_name(self, scale_line):
+        parts = scale_line.split(" ")
+        return parts and parts[0] or ""
+
+    def get_scale_width(self, scale):
+        """ get width from allowed_scales line
+            large 800:65536
+        """
+        for s in self.allowed_scales:
+            parts = s.split(" ")
+            if not parts:
+                continue
+            if parts[0] == scale:
+                dimentions = parts[1].split(":")
+                if not dimentions:
+                    continue
+                return dimentions[0]
 
     def __call__(self, data):
         data = re.sub(r"<([^<>\s]+?)\s*/>", self._shorttag_replace, data)
@@ -83,39 +101,43 @@ class ImageSrcsetFilter(object):
                 )
             )
             return elem
-        images_scales = self.image_scales
-        excluded_scales = srcset_config.get("excludedScales")
-        if excluded_scales:
-            images_scales = [scale for scale in self.image_scales if not scale in excluded_scales]
+        allowed_scales = self.allowed_scales
         sourceset = srcset_config.get("sourceset")
         if not sourceset:
             return elem
         src = elem.attrs.get("src")
         picture_tag = soup.new_tag("picture")
         for i, source in enumerate(sourceset):
-            scale = source["scale"]
+            target_scale = source["scale"]
             media = source.get("media")
+
             additional_scales = source.get("additionalScales", None)
             if additional_scales is None:
-                additional_scales = [s for s in images_scales if s != scale]
-            if i == len(sourceset) - 1:
-                source_tag = soup.new_tag(
-                    "img", src=self.update_src_scale(src=src, scale=scale),
-
-                )
-            else:
-                # TODO guess type:
-                source_tag = soup.new_tag(
-                    "source", srcset=self.update_src_scale(src=src, scale=scale)
-                )
-            for k, attr in elem.attrs.items():
-                if k in ["src", "srcset"]:
-                    continue
-                source_tag.attrs[k] = attr
-            source_tag["loading"] = "lazy"
+                additional_scales = [self.get_scale_name(s) for s in allowed_scales if s != target_scale]
+            source_scales = [target_scale] + additional_scales
+            source_srcset = []
+            for scale in source_scales:
+                scale_url = self.update_src_scale(src=src, scale=scale)
+                scale_width = self.get_scale_width(scale)
+                source_srcset.append("{0} {1}w".format(scale_url, scale_width))
+            source_tag = soup.new_tag(
+                "source", srcset=",\n".join(source_srcset)
+            )
             if media:
                 source_tag["media"] = media
             picture_tag.append(source_tag)
+            if i == len(sourceset) - 1:
+                scale_width = self.get_scale_width(target_scale)
+                img_tag = soup.new_tag(
+                    "img", src=self.update_src_scale(src=src, scale=target_scale),
+                )
+                for k, attr in elem.attrs.items():
+                    if k in ["src", "srcset"]:
+                        continue
+                    img_tag.attrs[k] = attr
+                img_tag["width"] = scale_width
+                img_tag["loading"] = "lazy"
+                picture_tag.append(img_tag)
         return picture_tag
 
     def update_src_scale(self, src, scale):
