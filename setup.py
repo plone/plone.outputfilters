0c1be5196ceb3974fa73ce96511ae4a1228e6f16
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-03-14 22:59:49 +0100

    feat: pyroma

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
     namespace_packages=["plone"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.8",
     install_requires=[
         "Products.CMFCore",
         "Products.GenericSetup",
